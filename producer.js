7621c5da12ce97e22353bc479ddc83b07aeb64c0
Achmat Fajri <achmatfajri@gmail.com>
2024-03-12 15:22:13 +1400

    create producer

---
+++ producer.js
@@ -0,0 +1,15 @@
+import { connect } from "amqplib";
+
+const connection = await connect("amqp://guest:guest@localhost:5672/");
+const channel = await connection.createChannel()
+
+for(let i = 0; i < 10; i++){
+    channel.publish("notification", "email", Buffer.from(`ini email ke ${i}`), {
+        headers: {
+            'sample': 'value'
+        }
+    })
+}
+
+await channel.close()
+await connection.close()
